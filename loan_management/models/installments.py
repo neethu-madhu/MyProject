f5b09d99c8db1d15d83fc66d146522cb69475fd2
neethu madhu <neethumadhu15@gmail.com>
2021-01-08 00:12:21 +0530

    [ADD]Loan Management

--- loan_management/models/installments.py
+++ loan_management/models/installments.py
@@ -10,6 +10,8 @@ class LoanInstallments(models.Model):
     date = fields.Datetime(string="Payment Date", required=True, help="Date of the payment")
     amount = fields.Float(string="Amount", required=True, help="Amount")
     loan_id = fields.Many2one('loan.request', string="Loan Ref.", help="Loan")
+    # partner_id = fields.Char(string="Name", related='loan_id.partner_id')
+    # email = fields.Char(string="Email", related='loan_id.email')
     sequence_no = fields.Char(string="Seq", required=True, related='loan_id.sequence_no')
     state = fields.Selection([
         ('draft', 'Draft'),
--- loan_management/models/loan_request.py
+++ loan_management/models/loan_request.py
@@ -8,6 +8,17 @@ class LoanRequest(models.Model):
     _description = 'Loan Requests'
     _rec_name = 'sequence_no'
 
+    # def _compute_loan_amount(self):
+    #     total_paid = 0.0
+    #     for loan in self:
+    #         for line in loan.loan_lines:
+    #             if line.paid:
+    #                 total_paid += line.amount
+    #         balance_amount = loan.loan_amount - total_paid
+    #         loan.total_amount = loan.loan_amount
+    #         loan.balance_amount = balance_amount
+    #         loan.total_paid_amount = total_paid
+
     # fields
     sequence_no = fields.Char(string="Sequence Number",
                               readonly=True,
@@ -24,16 +35,31 @@ class LoanRequest(models.Model):
     approved_date = fields.Date(string='Approved Date', help='Date of approval', default=datetime.today())
     currency_id = fields.Many2one('res.currency',
                                   string="Currency")
-    principal_amount = fields.Monetary(string='Principal Amount')
-    total_loan_amount = fields.Float(string="Total Amount",
-                                     computte='_compute_total_amount')
+    principal_amount = fields.Monetary(string='Principal Amount', required=True)
+    # total_loan_amount = fields.Float(string="Total Amount", compute='_compute_total_amount')
     loan_schemes_id = fields.Many2one('loan.schemes',
                                       string='Scheme',
+                                      required=True,
                                       editable=False,
                                       help='Scheme of loan')
-    interest_rate = fields.Float(string='Rate', related='loan_schemes_id.interest_rate')
-    duration = fields.Integer(string='Duration',
-                                     related='loan_schemes_id.duration')
+    interest_amount = fields.Float(string="Interest amount",
+                                   required=True,
+                                   help="interest amount")
+    loan_amount = fields.Float(string="Loan Amount",
+                               required=True,
+                               help="Loan amount")
+    total_amount = fields.Float(string="Total Amount",
+                                store=True, readonly=True,
+                                help="Total loan amount")
+    monthly_payment = fields.Float(string="Monthly Payment")
+    # balance_amount = fields.Float(string="Balance Amount",
+    #                               store=True,
+    #                               compute='_compute_loan_amount',
+    #                               help="Balance amount")
+    # total_paid_amount = fields.Float(string="Total Paid Amount",
+    #                                  store=True,
+    #                                  compute='_compute_loan_amount',
+    #                                  help="Total paid amount")
     state = fields.Selection([
         ('draft', 'Draft'),
         ('sent', 'Request Sent '),
@@ -41,6 +67,20 @@ class LoanRequest(models.Model):
         ('refuse', 'Refused'),
         ('cancel', 'Cancelled'),
     ], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
+
+    # installment line
+    loan_lines = fields.One2many('loan.request.line', 'loan_id', string="Loan Line", index=True)
+
+    # calculating interest amount toal amount to be paid and monthly payment
+    @api.onchange('loan_schemes_id')
+    def compute_interest_amount(self):
+        for rec in self:
+            interest_rate = rec.loan_schemes_id.interest_rate/100
+            principal_amount = rec.principal_amount
+            duration = rec.loan_schemes_id.duration
+            rec.interest_amount = principal_amount * interest_rate
+            rec.total_amount = principal_amount + rec.interest_amount
+            rec.monthly_payment = rec.total_amount / duration
 
     # action for Apply button
     def action_send_request(self):
@@ -71,3 +111,13 @@ class LoanRequest(models.Model):
                 'loan.request.sequence') or 'New'
         result = super(LoanRequest, self).create(values)
         return result
+
+
+class InstallmentLine(models.Model):
+    _name = 'loan.request.line'
+    _description = "Installment details"
+
+    date = fields.Date(string="Payment Date", required=True, help="Date of the payment")
+    loan_id = fields.Many2one('loan.request', string="Loan Ref.")
+    amount = fields.Float(string="Amount", required=True)
+    paid = fields.Boolean(string="Paid")
